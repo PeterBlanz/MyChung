0e4e0d0b403c0f80c92282245de0ab4bffbfdb5c
Peter Blanz <peter.blanz@outotec.com>
2024-06-14 22:43:43 +0300

    Implemented Chung analysis

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,70 @@ namespace MyChung
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.butBrowse = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.butProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butBrowse
+            // 
+            this.butBrowse.Location = new System.Drawing.Point(118, 29);
+            this.butBrowse.Name = "butBrowse";
+            this.butBrowse.Size = new System.Drawing.Size(75, 20);
+            this.butBrowse.TabIndex = 0;
+            this.butBrowse.Text = "...";
+            this.butBrowse.UseVisualStyleBackColor = true;
+            this.butBrowse.Click += new System.EventHandler(this.ButBrowse_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 29);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File name";
+            // 
+            // butProcess
+            // 
+            this.butProcess.Location = new System.Drawing.Point(12, 55);
+            this.butProcess.Name = "butProcess";
+            this.butProcess.Size = new System.Drawing.Size(181, 20);
+            this.butProcess.TabIndex = 3;
+            this.butProcess.Text = "Process";
+            this.butProcess.UseVisualStyleBackColor = true;
+            this.butProcess.Click += new System.EventHandler(this.ButProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butProcess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.butBrowse);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button butBrowse;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button butProcess;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Dynastream.Fit;
 
 namespace MyChung
 {
@@ -15,6 +17,109 @@ namespace MyChung
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ProcessFitFile(string fileName)
+        {
+            // create empty lists
+            List<double> timeValues = new List<double>();
+            List<double> powerValues = new List<double>();
+            List<double> speedValues = new List<double>();
+
+            // attempt to open .FIT file
+            using (FileStream fitSource = new FileStream(fileName, FileMode.Open))
+            {
+                // use a FitListener to capture all decoded messages in a FitMessages object
+                FitListener fitListener = new FitListener();
+                Decode decoder = new Decode();
+                decoder.MesgEvent += fitListener.OnMesg;
+                decoder.Read(fitSource);
+
+                // parse messages
+                FitMessages fitMessages = fitListener.FitMessages;
+                foreach (RecordMesg mesg in fitMessages.RecordMesgs)
+                {
+                    foreach (Field field in mesg.GetOverrideField(RecordMesg.FieldDefNum.Timestamp)) timeValues.Add(Convert.ToDouble(field.GetValue()));
+                    foreach (Field field in mesg.GetOverrideField(RecordMesg.FieldDefNum.Power)) powerValues.Add(Convert.ToDouble(field.GetValue()));
+                    foreach (Field field in mesg.GetOverrideField(RecordMesg.FieldDefNum.Speed)) speedValues.Add(Convert.ToDouble(field.GetValue()));
+                }
+            }
+
+            // analyze
+            DoChungAnalysis(timeValues, powerValues, speedValues);
+        }
+
+        private void DoChungAnalysis(List<double> timeValues, List<double> powerValues, List<double> speedValues)
+        {
+            if (timeValues.Count != powerValues.Count || timeValues.Count != speedValues.Count)
+            {
+                MessageBox.Show("Data set lengths must match!");
+                return;
+            }
+
+            // get parameters
+            ChungParameters chungParams = new ChungParameters();
+            chungParams.AirDensity = 1.2215;
+            chungParams.Mass = 90.55;
+            chungParams.RollingResistance = 0.0028;
+            chungParams.CdA = 0.25;
+
+            // solve CdA
+            double totalVirtualElevation = GetTotalVirtualElevation(chungParams, timeValues, powerValues, speedValues);
+            int iterationDirection = Math.Sign(totalVirtualElevation);
+            for(int i = 0; i < 1000000; i++)
+            {
+                if (Math.Sign(totalVirtualElevation) != iterationDirection) break;
+                chungParams.CdA += iterationDirection * 0.000001;
+                totalVirtualElevation = GetTotalVirtualElevation(chungParams, timeValues, powerValues, speedValues);
+            }
+            MessageBox.Show(chungParams.CdA.ToString());
+        }
+
+        private double GetTotalVirtualElevation(ChungParameters chungParams, List<double> timeValues, List<double> powerValues, List<double> speedValues)
+        {
+            double totalVirtualElevation = 0;
+            for (int i = 1; i < timeValues.Count - 1; i++)
+            {
+                totalVirtualElevation += GetVirtualElevation(chungParams, timeValues, powerValues, speedValues, i);
+            }
+            return totalVirtualElevation;
+        }
+
+        private double GetVirtualElevation(ChungParameters chungParams, List<double> timeValues, List<double> powerValues, List<double> speedValues, int i)
+        {
+            double dt = 0.5 * (timeValues[i + 1] - timeValues[i - 1]);
+            double a = (speedValues[i + 1] - speedValues[i - 1]) / (2 * dt);
+            double v = speedValues[i];
+            double Crr = chungParams.RollingResistance;
+            double CdA = chungParams.CdA;
+            double m = chungParams.Mass;
+            double w = powerValues[i];
+            double rho = chungParams.AirDensity;
+            const double g = 9.81;
+            double s = w / (m * g * v) - Crr - a / g - (rho * CdA * v * v) / (2 * m * g);
+
+            return s * v * dt;
+        }
+
+
+        private void ButBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Title = "Select FIT file", Filter = "FIT files|*.fit|All files|*.*" };
+            if (ofd.ShowDialog() == DialogResult.OK) txtFileName.Text = ofd.FileName;
+        }
+
+        private void ButProcess_Click(object sender, EventArgs e)
+        {
+            ProcessFitFile(txtFileName.Text);
+        }
+
+        private class ChungParameters
+        {
+            public double Mass { get; set; }
+            public double RollingResistance { get; set; }
+            public double AirDensity { get; set; }
+            public double CdA { get; set; }
         }
     }
 }
