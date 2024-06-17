d344cc3001a38b8ef73041ebb0445cc5040c142e
Peter Blanz <peter.blanz@outotec.com>
2024-06-17 14:37:37 +0300

    Added a form for calculating air density

---
+++ DensityForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace MyChung
+{
+    partial class DensityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHumidity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAirPressure = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butOK = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHumidity
+            // 
+            this.txtHumidity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHumidity.Location = new System.Drawing.Point(12, 123);
+            this.txtHumidity.Name = "txtHumidity";
+            this.txtHumidity.Size = new System.Drawing.Size(345, 20);
+            this.txtHumidity.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Relative humidity (%)";
+            // 
+            // txtAirPressure
+            // 
+            this.txtAirPressure.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAirPressure.Location = new System.Drawing.Point(12, 74);
+            this.txtAirPressure.Name = "txtAirPressure";
+            this.txtAirPressure.Size = new System.Drawing.Size(345, 20);
+            this.txtAirPressure.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Air pressure (hPa)";
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTemperature.Location = new System.Drawing.Point(12, 25);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(345, 20);
+            this.txtTemperature.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Temperature (°C)";
+            // 
+            // butOK
+            // 
+            this.butOK.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.butOK.Location = new System.Drawing.Point(12, 197);
+            this.butOK.Name = "butOK";
+            this.butOK.Size = new System.Drawing.Size(165, 37);
+            this.butOK.TabIndex = 19;
+            this.butOK.Text = "OK";
+            this.butOK.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(192, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 37);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // DensityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 246);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.butOK);
+            this.Controls.Add(this.txtHumidity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAirPressure);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.label3);
+            this.MinimumSize = new System.Drawing.Size(385, 285);
+            this.Name = "DensityForm";
+            this.ShowIcon = false;
+            this.Text = "Air density calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHumidity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAirPressure;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butOK;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ DensityForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyChung
+{
+    public partial class DensityForm : Form
+    {
+        public DensityForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
